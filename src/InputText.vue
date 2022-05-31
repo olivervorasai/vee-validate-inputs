3329ace3eafe7131c7de3ec4565c492c17a6e25e
Oliver Vorasai <olivervorasai@gmail.com>
2022-05-30 21:31:05 -0700

    feat(components): add content to components

--- src/InputText.vue
+++ src/InputText.vue
@@ -1,5 +1,34 @@
-<script setup></script>
+<script setup lang="ts">
+import { useField } from "vee-validate";
+
+const props = withDefaults(
+    defineProps<{
+        disabled?: boolean;
+        label?: string;
+        name: string;
+        showChanged?: boolean;
+        type: string;
+        vertical?: boolean;
+    }>(),
+    {
+        disabled: false,
+        label: "",
+        showChanged: false,
+        vertical: false,
+    }
+);
+
+const { value, handleBlur, handleChange, meta } = useField<string>(props.name);
+</script>
 
 <template>
-    <input text="text" />
+    <input
+        :id="name"
+        :value="value"
+        :name="name"
+        :type="type"
+        :disabled="disabled"
+        @input="handleChange"
+        @blur="handleBlur"
+    />
 </template>
--- src/InputTextarea.vue
+++ src/InputTextarea.vue
@@ -1,5 +1,35 @@
-<script setup></script>
+<script setup lang="ts">
+import { useField } from "vee-validate";
+
+const props = withDefaults(
+    defineProps<{
+        disabled?: boolean;
+        height?: number;
+        label?: string;
+        name: string;
+        showChanged?: boolean;
+        vertical?: boolean;
+    }>(),
+    {
+        disabled: false,
+        height: 5,
+        label: "",
+        showChanged: false,
+        vertical: false,
+    }
+);
+
+const { value, handleBlur, handleChange, meta } = useField<string>(props.name);
+</script>
 
 <template>
-    <textarea></textarea>
+    <textarea
+        :id="name"
+        :value="value"
+        :name="name"
+        :disabled="disabled"
+        :rows="height"
+        @input="handleChange"
+        @blur="handleBlur"
+    ></textarea>
 </template>
