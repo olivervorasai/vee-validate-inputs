e145933e9250272f6622276c396ef31feac93f56
Oliver Vorasai <olivervorasai@gmail.com>
2022-06-04 23:00:29 -0700

    fix(css): add missing shadows and transitions

--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -1,5 +1,7 @@
-import '../../assets/css/base.css';
 import '../../assets/css/colors.css';
+import '../../assets/css/shadows.css';
+import '../../assets/css/transitions.css';
+import '../../assets/css/base.css';
 
 import InputText from './InputText.vue';
 import InputTextarea from './InputTextarea.vue';
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import './assets/css/base.css';
 import './assets/css/colors.css';
+import './assets/css/shadows.css';
+import './assets/css/transitions.css';
+import './assets/css/base.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
