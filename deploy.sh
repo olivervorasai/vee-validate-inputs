7db0b5efb95e9f7b90f25ad79e0992d7ba93c677
Oliver Vorasai <olivervorasai@gmail.com>
2022-06-08 21:47:17 -0700

    ci: deploy docs not app

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,15 @@
 set -e
 
 # build
-npm run build
+npm run docs:build
 
 # navigate into the build output directory
-cd dist
+cd docs/.vitepress/dist
 
 git init
-git checkout -b main
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:olivervorasai/vee-validate-input.git main:gh-pages
+git push -f git@github.com:olivervorasai/vee-validate-inputs.git main:gh-pages
 cd -
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+    base: '/vee-validate-inputs/',
     title: 'VeeValidate Inputs',
     description: 'Just playing around.',
     themeConfig: {
